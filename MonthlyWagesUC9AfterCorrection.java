934ac8f8a896cfac39903d7d18efd424472ddf13
shobhit-ranjan <shobhit.ranjan03@gmail.com>
2021-08-09 11:23:18 +0530

    UC9 Emp Salary Of a Month In A company AFTER CORRECTION ONLY INE CLASS IN ONE FILE

---
+++ MonthlyWagesUC9AfterCorrection.java
@@ -0,0 +1,14 @@
+package Day10_Emp_Wages;
+
+public class MonthlyWagesUC9AfterCorrection {
+
+	public static void main(String[] args) {
+	
+
+		System.out.println("Welcome to  Emp Salary computation Problem");
+		TotalWages Employee = new TotalWages();
+		System.out.println("Monthly wages of an Employee is " + Employee);
+
+	}
+
+}
